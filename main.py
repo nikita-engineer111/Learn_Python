bce064cd1e18941c5fe0b41a0d23d3545168712b
nikita-engineer111 <45405597+nikita-engineer111@users.noreply.github.com>
2022-12-02 15:38:01 +0300

    added new task and answer

--- main.py
+++ main.py
@@ -506,3 +506,40 @@ print(generate_access_config())
 
 ################################################################
 
+"""
+Задание 7.1a
+Сделать копию скрипта задания 7.1
+Изменить скрипт таким образом, чтобы функция возвращала не список команд, а
+словарь:
+ключи: имена интерфейсов, вида 'FastEthernet0/12'
+значения: список команд, который надо выполнить на этом интерфейсе
+"""
+# ANSWER
+"""
+access_template = ['switchport mode access',
+'switchport access vlan',
+'switchport nonegotiate',
+'spanning-tree portfast',
+'spanning-tree bpduguard enable']
+
+access_dict = { 'FastEthernet0/12':10,
+'FastEthernet0/14':11,
+'FastEthernet0/16':17,
+'FastEthernet0/17':150 }
+
+def generate_access_config():
+    config = {}
+    for interface in access_dict.keys():
+        config[interface] = []
+        for command in access_template:
+            if command.endswith('access vlan'):
+                config[interface].append('%s %s' % (command, access_dict[interface]))
+            else:
+                config[interface].append('%s ' % command)
+    return config
+
+print(generate_access_config())
+"""
+
+################################################################
+
