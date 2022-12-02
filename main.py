0953a9f84b0b2a5531b0a15985e7a6aea04eae23
nikita-engineer111 <45405597+nikita-engineer111@users.noreply.github.com>
2022-12-02 15:29:09 +0300

    added new task and answer

--- main.py
+++ main.py
@@ -465,3 +465,44 @@ for int in fast_int['trunk']:
 
 ################################################################
 
+"""
+Задание 7.1
+Создать функцию, которая генерирует конфигурацию для access-портов.
+Параметр access ожидает, как аргумент, словарь access-портов, вида:
+{'FastEthernet0/12':10,
+'FastEthernet0/14':11,
+'FastEthernet0/16':17,
+'FastEthernet0/17':150}
+Функция должна возвращать список всех портов в режиме access с конфигурацией на
+основе шаблона access_template.
+В конце строк в списке не должно быть символа перевода строки.
+"""
+# ANSWER
+"""
+access_template = ['switchport mode access',
+'switchport access vlan',
+'switchport nonegotiate',
+'spanning-tree portfast',
+'spanning-tree bpduguard enable']
+
+access_dict = { 'FastEthernet0/12':10,
+'FastEthernet0/14':11,
+'FastEthernet0/16':17,
+'FastEthernet0/17':150 }
+
+def generate_access_config():
+    config = []
+    for interface in access_dict.keys():
+        config.append(interface)
+        for command in access_template:
+            if command.endswith('access vlan'):
+                config.append('%s %s' % (command, access_dict[interface]))
+            else:
+                config.append('%s ' % command)
+    return config
+
+print(generate_access_config())
+"""
+
+################################################################
+
